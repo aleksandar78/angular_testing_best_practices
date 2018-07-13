ab6697b9c6cc3c93e316bae342ffbdff03073d5e
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2018-07-14 00:45:37 +0200

    Fix wrong title made by angular cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +11,17 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'tdd'`, async(() => {
+  it(`should have as title 'angular-testing-best-practices'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tdd');
+    expect(app.title).toEqual('angular-testing-best-practices');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-testing-best-practices!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to angular-testing-best-practices!'
+    );
   }));
 });
